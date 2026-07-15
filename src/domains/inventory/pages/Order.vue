dbe279c9b5a58d104c7536efae792bce5ef11b58
dennisrev <65338270+dennisrev@users.noreply.github.com>
2026-07-15 13:53:45 +0200

    Update Order.vue

--- src/domains/inventory/pages/Order.vue
+++ src/domains/inventory/pages/Order.vue
@@ -4,11 +4,7 @@ import ProductTable from '../components/ProductTable.vue';
 
 import { getLowStockProducts } from '../store.ts';
 
-// we willen lowStockProducts
-
 const lowStockProducts = getLowStockProducts; 
-
-// we willlen hier nu alleen producten waarvan te weining zijn
 
 </script>
 
